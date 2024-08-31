d919e44fb37677927c2be3a75b2759727f5db6a9
Jakob <jnunnendorf@icloud.com>
2024-08-31 15:55:35 +0800

    feat(Industries.tsx): add to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Companies from '@/components/Companies/Companies'
 import Hero from '@/components/Hero/Hero'
+import Industries from '@/components/Industries'
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
       <Companies />
+      <Industries />
     </main>
   )
 }
---
+++ components/Industries.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Industries() {
+  return (
+    <div className="rborder grid h-screen w-full place-content-center">
+      Industries
+    </div>
+  )
+}
