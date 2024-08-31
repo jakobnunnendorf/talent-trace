ad64e7c556aabb99c4acaeb3aa213f7f87079176
Jakob <jnunnendorf@icloud.com>
2024-08-31 17:33:49 +0800

    chore(tailwind.config.ts): extend text sizes up to 10-12xl

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        '10xl': '9rem',
+        '11xl': '10rem',
+        '12xl': '11rem',
       },
     },
   },
