03c7a0e52fc7bd96c65d651dd3008b5f793ed4a3
Jakob <jnunnendorf@icloud.com>
2024-09-14 16:54:51 +0800

    style(companies.tsx): make responsive

--- components/Companies/Companies.tsx
+++ components/Companies/Companies.tsx
@@ -12,48 +12,33 @@ export default function Companies() {
     '/Glencore logo.png',
   ]
   return (
-    <div className="grid h-96 w-full grid-rows-4 text-center">
-      <div className="row-start-2">
+    <div className="w-full overflow-hidden py-8 text-center md:grid md:h-96 md:grid-rows-4 md:py-0">
+      <div className="row-start-2 w-screen">
         <h2 className="text-3xl">Top Companies hiring at Talent Trace</h2>
-        <h4 className="text-md mt-1 text-gray-500">
+        <h4 className="text-md mt-1 py-4 text-gray-500 md:py-0">
           We have longstanding business relations with
         </h4>
       </div>
-      <div className="row-start-3 mx-auto flex w-[1250px] overflow-hidden">
-        <ul className="slide-left flex h-full">
-          {companies.map((company, index) => (
-            <li
-              key={index}
-              className={`ml-[100px] w-[150px] ${index % 2 == 1 ? 'bbobrder' : 'gborbder'} flex-shrink-0`}
-            >
-              <figure className="relative h-full w-full">
-                <Image
-                  className="object-contain"
-                  src={company}
-                  fill
-                  alt={company}
-                />
-              </figure>
-            </li>
-          ))}
-        </ul>
-        <ul className="slide-left flex h-full">
-          {companies.map((company, index) => (
-            <li
-              key={index}
-              className={`ml-[100px] w-[150px] ${index % 2 == 1 ? 'b' : 'g'} flex-shrink-0`}
-            >
-              <figure className="relative h-full w-full">
-                <Image
-                  className="object-contain"
-                  src={company}
-                  fill
-                  alt={company}
-                />
-              </figure>
-            </li>
-          ))}
-        </ul>
+      <div className="row-start-3 mx-auto flex w-[1250px] py-4">
+        {[...Array(2)].map((_, listIndex) => (
+          <ul key={listIndex} className="slide-left flex h-full">
+            {companies.map((company, index) => (
+              <li
+                key={`${listIndex}-${index}`}
+                className="ml-[100px] h-24 w-[150px] flex-shrink-0 md:h-auto"
+              >
+                <figure className="relative h-full w-full">
+                  <Image
+                    className="object-contain"
+                    src={company}
+                    fill
+                    alt={`Logo of ${company.split('/').pop()?.split('.')[0]}`}
+                  />
+                </figure>
+              </li>
+            ))}
+          </ul>
+        ))}
       </div>
     </div>
   )
