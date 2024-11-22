52a2a5b95dfc566af5aea4eebbcd607f73ea5927
Jakob <jnunnendorf@icloud.com>
2024-11-22 21:46:47 +0800

    add bounce

--- components/NavBar/UploadCV.tsx
+++ components/NavBar/UploadCV.tsx
@@ -2,15 +2,19 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-export default function UploadCV() {
+interface UploadCVProps {
+  scrollDirection: 'up' | 'down'
+}
+
+export default function UploadCV({ scrollDirection }: UploadCVProps) {
   return (
-    <Link 
+    <Link
       href="https://talent-trace.zohorecruit.com/forms/677184c20dcfb9044e43b5672ceb5e3530c617412e8922022dcc64355524d8f5"
       target="_blank"
       rel="noopener noreferrer"
-      className="flex w-fit items-center justify-center gap-2 lg:w-32 lg:border hover:opacity-80 transition-opacity"
+      className={` ${scrollDirection === 'up' ? 'mx-auto w-fit lg:w-36' : 'mr-auto w-64 px-12'} bounce-animation flex h-[58px] items-center justify-center gap-2 rounded-full bg-blue/80 px-6 shadow-xl backdrop-blur-md transition-opacity hover:opacity-80`}
     >
-      <figure className="flex flex-col items-center gap-1">
+      <figure className="flex flex-col items-center gap-1 text-white">
         <Image
           className="lg:hidden"
           src="/upload cv icon.png"
