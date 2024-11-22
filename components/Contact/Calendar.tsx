2282f819d9084c60a0baab691b2f3f79dae73b47
Jakob <jnunnendorf@icloud.com>
2024-11-22 15:27:33 +0800

    replace calendar widget with shadcn calendar

--- components/Contact/Calendar.tsx
+++ components/Contact/Calendar.tsx
@@ -1,37 +1,18 @@
-import React from 'react'
+'use client'
 
-export default function Calendar() {
+import * as React from 'react'
+
+import { Calendar } from '@/components/ui/calendar'
+
+export default function CalendarDemo() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
+
   return (
-    <div className="ml-auto mr-4 w-96 rounded-3xl bg-white p-6 drop-shadow-2xl">
-      <div className="overflow-hidden rounded-lg">
-        <div className="py-2 text-center opacity-40">
-          <h4 className="text-xl font-semibold text-[#828282]">June 2023</h4>
-        </div>
-        <table className="w-full">
-          <thead>
-            <tr className="border-t">
-              <th className="px-1 py-2 text-xs">Su</th>
-              <th className="px-1 py-2 text-xs">Mo</th>
-              <th className="px-1 py-2 text-xs">Tu</th>
-              <th className="px-1 py-2 text-xs">We</th>
-              <th className="px-1 py-2 text-xs">Th</th>
-              <th className="px-1 py-2 text-xs">Fr</th>
-              <th className="px-1 py-2 text-xs">Sa</th>
-            </tr>
-          </thead>
-          <tbody>
-            {[...Array(5)].map((_, weekIndex) => (
-              <tr key={weekIndex}>
-                {[...Array(7)].map((_, dayIndex) => (
-                  <td key={dayIndex} className="px-1 py-2 text-center text-sm">
-                    {((weekIndex * 7 + dayIndex) % 31) + 1}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="w-fit rounded-md border"
+    />
   )
 }
--- components/Contact/ContactUs.tsx
+++ components/Contact/ContactUs.tsx
@@ -8,8 +8,10 @@ export default function ContactUs() {
     <div className="min-h-screen bg-white px-4 py-24">
       <div className="mx-auto">
         <ContactHeader />
-        <div className="grid grid-cols-1 gap-24 md:grid-cols-2">
-          <Calendar />
+        <div className=" grid grid-cols-1 gap-24 md:grid-cols-2">
+          <div className=" flex justify-end p-6">
+            <Calendar />
+          </div>
           <ScheduleCall />
         </div>
       </div>
