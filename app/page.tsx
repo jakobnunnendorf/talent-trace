a7fe98eaeddcdb7e36ae68c27c041dbccec1412e
Jakob <jnunnendorf@icloud.com>
2024-11-22 15:20:05 +0800

    add preview

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,19 @@ import Industries from '@/components/Industries/Industries'
 import JobPreview from '@/components/Jobs/JobPreview'
 import Services from '@/components/Services/Services'
 import Testimonials from '@/components/Testimonials/Testimonials'
+import ZohoRecruitWidget from '@/components/ZohoRecuritWidget'
+import ZohoRecruitWidgetStyle1 from '@/components/ZohoRecuritWidget2'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
       <Companies />
+      {/* <ZohoRecruitWidget /> */}
+      <ZohoRecruitWidgetStyle1 />
       <Industries />
-      <JobPreview />
+      {/* <JobPreview /> */}
       <Services />
       <Testimonials />
       <ContactUs />
