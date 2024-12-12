74dbdfdddad76676d8c160dbe64eea12d4824e67
Jakob <jnunnendorf@icloud.com>
2024-12-12 13:05:38 -1000

    adjust import link

--- components/Hero/Background.tsx
+++ components/Hero/Background.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Background() {
   return (
     <figure className="drift-right absolute -z-50 h-full w-[150vh] md:w-[150vw]">
-      <Image src="/Hero-extended.png" alt="ocean background" fill />
+      <Image src="/hero/Hero-extended.png" alt="ocean background" fill />
     </figure>
   )
 }
