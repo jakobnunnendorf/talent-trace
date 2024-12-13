84ff3d280fb0728dffed8d1825d93bc7db02b135
Jakob <jnunnendorf@icloud.com>
2024-12-12 14:17:52 -1000

    finish about us page

--- app/explore/about-us/page.tsx
+++ app/explore/about-us/page.tsx
@@ -1,4 +1,5 @@
 import SubHero from '@/components/Header/SubHero'
+import Image from 'next/image'
 import React from 'react'
 
 const AboutUsPage: React.FC = () => {
@@ -92,19 +93,31 @@ const AboutUsPage: React.FC = () => {
 
       {/* Message from Managing Director */}
       <section className="bg-gray-100 px-6 py-12">
-        <h2 className="text-center text-3xl font-bold">
-          Message from Our Managing Director
-        </h2>
-        <div className="mx-auto mt-6 max-w-3xl text-center">
-          <p className="text-lg italic">
-            &quot;At Talent Trace, our number one priority is to deliver
-            exceptional service to both our clients and candidates. Whether
-            you&apos;re hiring talent or seeking a new opportunity, our Talent
-            Tracers are here to ensure a seamless experience at every
-            step.&quot;
-          </p>
-          <p className="mt-4 font-bold">– Pravin Nyanes</p>
-          <p className="italic">Managing Director, Talent Trace</p>
+        <div className="mx-auto grid w-2/3 grid-cols-2">
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-center text-3xl font-bold">
+              Message from Our Managing Director
+            </h2>
+            <div className="mx-auto mt-6 max-w-3xl text-center">
+              <p className="text-lg italic">
+                &quot;At Talent Trace, our number one priority is to deliver
+                exceptional service to both our clients and candidates. Whether
+                you&apos;re hiring talent or seeking a new opportunity, our
+                Talent Tracers are here to ensure a seamless experience at every
+                step.&quot;
+              </p>
+              <p className="mt-4 font-bold">– Pravin Nyanes</p>
+              <p className="italic">Managing Director, Talent Trace</p>
+            </div>
+          </div>
+          <figure className="flex items-center justify-center">
+            <Image
+              src="/explore/about-us/pravin.png"
+              width={300}
+              height={300}
+              alt="Pravin Nyanes"
+            />
+          </figure>
         </div>
       </section>
 
