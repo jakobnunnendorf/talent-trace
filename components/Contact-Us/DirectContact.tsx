be949c2308538d92be401547629a7b22050277c0
Jakob <jnunnendorf@icloud.com>
2024-12-25 22:16:17 +0200

    improve spacing

--- components/Contact-Us/DirectContact.tsx
+++ components/Contact-Us/DirectContact.tsx
@@ -5,7 +5,7 @@ import ContactInformation from './ContactInformation'
 export default function DirectContact() {
   return (
     <div className="lg:col-span-2 lg:pt-8">
-      <div className="space-y-8">
+      <div className="space-y-8 px-8">
         <h2 className="text-2xl font-bold">Prefer to Reach Us Directly?</h2>
         <ContactInformation />
         <FAQs />
