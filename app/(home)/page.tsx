aa34743ac1ae710129a02494c93d35788f905064
Jakob <jnunnendorf@icloud.com>
2025-02-03 02:45:58 +0800

    fix revalidate more often

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,7 +7,7 @@ import NewsCarousel from '@/components/Home/Industries/NewsCarousel'
 import Services from '@/components/Home/Services/Services'
 import Testimonials from '@/components/Home/Testimonials/Testimonials'
 
-export const revalidate = 3000
+export const revalidate = 300
 
 export default function Home() {
   return (
