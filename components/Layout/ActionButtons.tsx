aaefbf27bf8f17d39de1dbbf9fa0ae46bea704c2
Jakob Nunnendorf <Jnunnendorf@icloud.com>
2025-03-10 11:52:51 +0800

    fix action button layout

--- components/Layout/ActionButtons.tsx
+++ components/Layout/ActionButtons.tsx
@@ -3,7 +3,7 @@ import Socials from '../shared/Buttons/Socials'
 
 export default function ActionButtons() {
   return (
-    <div className="fixed right-4 top-2/3 z-50 -translate-y-1/3 md:right-12">
+    <div className="fixed bottom-4 right-4 z-50 md:bottom-12 md:right-12">
       <Socials vertical color="blue" size="3" />
     </div>
   )
