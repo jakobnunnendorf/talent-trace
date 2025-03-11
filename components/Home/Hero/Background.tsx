88ee22a125118a30495638db681dd2ff5ad0cb1e
Jakob Nunnendorf <Jnunnendorf@icloud.com>
2025-03-11 18:47:18 +0800

    make LCP priority

--- components/Home/Hero/Background.tsx
+++ components/Home/Hero/Background.tsx
@@ -5,6 +5,7 @@ export default function Background() {
   return (
     <figure className="drift-right absolute -z-50 h-full w-[150vh] md:w-[150vw]">
       <Image
+        priority
         src="/hero/Hero-extended.png"
         alt="ocean background"
         fill
